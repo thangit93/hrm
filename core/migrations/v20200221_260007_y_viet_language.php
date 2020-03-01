51be80c8f0893d32d96be2e7a5af4a1a9abc27ac
Ha Tran <tm.ha@tego.global>
2020-03-02 02:22:13 +0700

    update translate and query translate.

--- core/migrations/v20200221_260007_y_viet_language.php
+++ core/migrations/v20200221_260007_y_viet_language.php
@@ -18,6 +18,12 @@ class v20200221_260007_y_viet_language extends AbstractMigration{
         $supportedLanguage->description = 'Vietnamese';
         $supportedLanguage->Save();
 
+        $sql = "update Users set `lang`=(select id from $tableName where name = 'vn') where lang != (select id from $tableName where name = 'vn');";
+        $this->executeQuery($sql);
+
+        $sql = "update Settings set `value`='vn' where name='System: Language'";
+        $this->executeQuery($sql);
+
     }
 
     public function down(){
