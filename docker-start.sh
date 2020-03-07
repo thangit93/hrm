67562e2a60cc1396456ea46ff6a627f2546974d6
Nhat Do <ddnhat@gmail.com>
2020-03-07 23:40:57 +0700

    remove docker-compose.yml

--- docker-start.sh
+++ docker-start.sh
@@ -3,8 +3,8 @@
 git pull
 
 echo "STARTING DOCKER ENVIRONMENT..."
-#docker-compose up -d --build
-docker-compose up -d
+docker-compose up -d $*
+#docker-compose up -d
 
 #echo "INSTALLING ICE HRM"
 #docker-compose exec -T php bash -c '/init-icehrm.sh'
