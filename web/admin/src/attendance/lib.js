8dc065e9eebd2b104af3db85da2734c8cd4636d8
Nhat Do <ddnhat@gmail.com>
2020-04-19 22:55:04 +0700

    resolves YVHM-73

--- web/admin/src/attendance/lib.js
+++ web/admin/src/attendance/lib.js
@@ -94,12 +94,12 @@ class AttendanceAdapter extends AdapterBase {
       if (cell === '0000-00-00 00:00:00' || cell === '' || cell === undefined || cell == null) {
         return '';
       }
-      return Date.parse(cell).toString('yyyy MMM d  <b>HH:mm</b>');
+      return Date.parse(cell).toString('d/M/yyyy  <b>HH:mm</b>');
     } if (id === 3) {
       if (cell === '0000-00-00 00:00:00' || cell === '' || cell === undefined || cell == null) {
         return '';
       }
-      return Date.parse(cell).toString('MMM d  <b>HH:mm</b>');
+      return Date.parse(cell).toString('d/M/yyyy  <b>HH:mm</b>');
     } if (id === 4) {
       if (cell !== undefined && cell !== null) {
         if (cell.length > 10) {
