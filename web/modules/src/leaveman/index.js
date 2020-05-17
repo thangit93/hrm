6c3e832dd6fe0a1db47b1dfdb38f0c6a1cedae92
Ha Tran <tm.ha@tego.global>
2020-05-17 11:19:56 +0700

    add list leave request and fix leave module.

--- web/modules/src/leaveman/index.js
+++ web/modules/src/leaveman/index.js
@@ -2,8 +2,10 @@ import {
   EmployeeLeaveAdapter,
   EmployeeLeaveEntitlementAdapter,
   SubEmployeeLeaveAdapter,
+  EmployeeLeaveBalanceAdapter,
 } from './lib';
 
 window.EmployeeLeaveAdapter = EmployeeLeaveAdapter;
 window.EmployeeLeaveEntitlementAdapter = EmployeeLeaveEntitlementAdapter;
 window.SubEmployeeLeaveAdapter = SubEmployeeLeaveAdapter;
+window.EmployeeLeaveBalanceAdapter = EmployeeLeaveBalanceAdapter;
