c9feb94f35614d7a8489cae73bc53d0d48fd80d1
NhatDo <ddnhat@gmail.com>
2020-05-18 23:53:37 +0700

    report employee

--- core/migrations/v20200518_225712_update_custom_fields.php
+++ core/migrations/v20200518_225712_update_custom_fields.php
@@ -8,6 +8,9 @@ class v20200518_225712_update_custom_fields extends AbstractMigration
     {
         $sql = "
 UPDATE CustomFields SET field_label = \"Số tài khoản\" WHERE `name`=\"bank_account\";
+";
+        $this->executeQuery($sql);
+        $sql = "
 UPDATE CustomFields SET field_label = \"Tên ngân hàng\" WHERE `name`=\"bank_name\";
 ";
         $this->executeQuery($sql);
