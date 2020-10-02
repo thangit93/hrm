bca98deb67b1cd39e0630aeb90136e60f638eaba
ducnhat <ddnhat@gmail.com>
2020-10-02 11:30:25 +0700

    update fb

--- web/modules/src/overtime/lib.js
+++ web/modules/src/overtime/lib.js
@@ -267,6 +267,45 @@ class EmployeeOvertimeAdapter extends ApproveModuleAdapter {
 
     this.postRenderForm(object, $tempDomObj);
   }
+
+  getActionButtonsHtml(id, data) {
+    console.log(data, this.user);
+    const editButton = '<img class="tableActionButton" src="_BASE_images/edit.png" style="cursor:pointer;" rel="tooltip" title="Edit" onclick="modJs.edit(_id_);return false;"></img>';
+    const deleteButton = '<img class="tableActionButton" src="_BASE_images/delete.png" style="margin-left:15px;cursor:pointer;" rel="tooltip" title="Delete" onclick="modJs.deleteRow(_id_);return false;"></img>';
+    const requestCancellationButton = `<img class="tableActionButton" src="_BASE_images/delete.png" style="margin-left:15px;cursor:pointer;" rel="tooltip" title="Cancel ${this.itemName}" onclick="modJs.cancelRequest(_id_);return false;"></img>`;
+    const viewLogsButton = '<img class="tableActionButton" src="_BASE_images/log.png" style="margin-left:15px;cursor:pointer;" rel="tooltip" title="View Logs" onclick="modJs.getLogs(_id_);return false;"></img>';
+
+
+    let html = '<div style="width:120px;">_edit__logs__delete_</div>';
+
+    html = html.replace('_logs_', viewLogsButton);
+
+    if (this.showDelete) {
+      if (data[7] === 'Approved') {
+        html = html.replace('_delete_', requestCancellationButton);
+      } else if (data[7] === 'Pending' || this.user.user_level === 'Admin') {
+        html = html.replace('_delete_', deleteButton);
+      } else {
+        html = html.replace('_delete_', '');
+      }
+    } else {
+      html = html.replace('_delete_', '');
+    }
+
+    if (this.showEdit) {
+      if (data[7] === 'Pending') {
+        html = html.replace('_edit_', editButton);
+      } else {
+        html = html.replace('_edit_', '');
+      }
+    } else {
+      html = html.replace('_edit_', '');
+    }
+
+    html = html.replace(/_id_/g, id);
+    html = html.replace(/_BASE_/g, this.baseUrl);
+    return html;
+  }
 }
 
 /*
