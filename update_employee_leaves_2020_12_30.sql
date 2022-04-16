8918aaab7f30db4a576e5b14e16aaf9c9de1248b
Thang Tran <thangtd1993@gmail.com>
2022-04-16 17:16:47 +0700

    first commit

---
+++ update_employee_leaves_2020_12_30.sql
@@ -0,0 +1,199 @@
+insert into EmployeeLeaves (employee, leave_type, leave_period, date_start, date_end, status, details) values (402,1,6,'2020-12-01','2020-12-02','Approved', 'Cập Nhật Ngày Nghỉ Nhân Viên');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1505, '2020-12-01', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1505, '2020-12-02', 'Half Day - Morning');
+insert into EmployeeLeaves (employee, leave_type, leave_period, date_start, date_end, status, details) values (403,1,6,'2020-12-01','2020-12-05','Approved', 'Cập Nhật Ngày Nghỉ Nhân Viên');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1506, '2020-12-01', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1506, '2020-12-02', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1506, '2020-12-03', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1506, '2020-12-04', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1506, '2020-12-05', 'Full Day');
+insert into EmployeeLeaves (employee, leave_type, leave_period, date_start, date_end, status, details) values (404,1,6,'2020-12-01','2020-12-05','Approved', 'Cập Nhật Ngày Nghỉ Nhân Viên');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1507, '2020-12-01', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1507, '2020-12-02', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1507, '2020-12-03', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1507, '2020-12-04', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1507, '2020-12-05', 'Half Day - Morning');
+insert into EmployeeLeaves (employee, leave_type, leave_period, date_start, date_end, status, details) values (405,1,6,'2020-12-01','2020-12-08','Approved', 'Cập Nhật Ngày Nghỉ Nhân Viên');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1508, '2020-12-01', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1508, '2020-12-02', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1508, '2020-12-03', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1508, '2020-12-04', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1508, '2020-12-05', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1508, '2020-12-06', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1508, '2020-12-07', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1508, '2020-12-08', 'Half Day - Morning');
+# insert into EmployeeLeaves (employee, leave_type, leave_period, date_start, date_end, status, details) values (406,1,6,'2020-12-01','2020-12-08','Approved', 'Cập Nhật Ngày Nghỉ Nhân Viên');
+insert into EmployeeLeaves (employee, leave_type, leave_period, date_start, date_end, status, details) values (407,1,6,'2020-12-01','2020-12-08','Approved', 'Cập Nhật Ngày Nghỉ Nhân Viên');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1509, '2020-12-01', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1509, '2020-12-02', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1509, '2020-12-03', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1509, '2020-12-04', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1509, '2020-12-05', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1509, '2020-12-06', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1509, '2020-12-07', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1509, '2020-12-08', 'Full Day');
+insert into EmployeeLeaves (employee, leave_type, leave_period, date_start, date_end, status, details) values (408,1,6,'2020-12-01','2020-12-06','Approved', 'Cập Nhật Ngày Nghỉ Nhân Viên');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1510, '2020-12-01', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1510, '2020-12-02', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1510, '2020-12-03', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1510, '2020-12-04', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1510, '2020-12-05', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1510, '2020-12-06', 'Full Day');
+insert into EmployeeLeaves (employee, leave_type, leave_period, date_start, date_end, status, details) values (409,1,6,'2020-12-01','2020-12-07','Approved', 'Cập Nhật Ngày Nghỉ Nhân Viên');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1511, '2020-12-01', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1511, '2020-12-02', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1511, '2020-12-03', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1511, '2020-12-04', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1511, '2020-12-05', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1511, '2020-12-06', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1511, '2020-12-07', 'Full Day');
+insert into EmployeeLeaves (employee, leave_type, leave_period, date_start, date_end, status, details) values (410,1,6,'2020-12-01','2020-12-08','Approved', 'Cập Nhật Ngày Nghỉ Nhân Viên');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1512, '2020-12-01', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1512, '2020-12-02', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1512, '2020-12-03', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1512, '2020-12-04', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1512, '2020-12-05', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1512, '2020-12-06', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1512, '2020-12-07', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1512, '2020-12-08', 'Full Day');
+insert into EmployeeLeaves (employee, leave_type, leave_period, date_start, date_end, status, details) values (411,1,6,'2020-12-01','2020-12-06','Approved', 'Cập Nhật Ngày Nghỉ Nhân Viên');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1513, '2020-12-01', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1513, '2020-12-02', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1513, '2020-12-03', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1513, '2020-12-04', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1513, '2020-12-05', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1513, '2020-12-06', 'Full Day');
+insert into EmployeeLeaves (employee, leave_type, leave_period, date_start, date_end, status, details) values (412,1,6,'2020-12-01','2020-12-04','Approved', 'Cập Nhật Ngày Nghỉ Nhân Viên');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1514, '2020-12-01', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1514, '2020-12-02', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1514, '2020-12-03', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1514, '2020-12-04', 'Half Day - Morning');
+insert into EmployeeLeaves (employee, leave_type, leave_period, date_start, date_end, status, details) values (419,1,6,'2020-12-01','2020-12-04','Approved', 'Cập Nhật Ngày Nghỉ Nhân Viên');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1515, '2020-12-01', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1515, '2020-12-02', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1515, '2020-12-03', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1515, '2020-12-04', 'Half Day - Morning');
+insert into EmployeeLeaves (employee, leave_type, leave_period, date_start, date_end, status, details) values (420,1,6,'2020-12-01','2020-12-01','Approved', 'Cập Nhật Ngày Nghỉ Nhân Viên');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1516, '2020-12-01', 'Full Day');
+insert into EmployeeLeaves (employee, leave_type, leave_period, date_start, date_end, status, details) values (425,1,6,'2020-12-01','2020-12-01','Approved', 'Cập Nhật Ngày Nghỉ Nhân Viên');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1517, '2020-12-01', 'Full Day');
+insert into EmployeeLeaves (employee, leave_type, leave_period, date_start, date_end, status, details) values (426,1,6,'2020-12-01','2020-12-03','Approved', 'Cập Nhật Ngày Nghỉ Nhân Viên');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1518, '2020-12-01', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1518, '2020-12-02', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1518, '2020-12-03', 'Full Day');
+insert into EmployeeLeaves (employee, leave_type, leave_period, date_start, date_end, status, details) values (427,1,6,'2020-12-01','2020-12-03','Approved', 'Cập Nhật Ngày Nghỉ Nhân Viên');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1519, '2020-12-01', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1519, '2020-12-02', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1519, '2020-12-03', 'Half Day - Morning');
+insert into EmployeeLeaves (employee, leave_type, leave_period, date_start, date_end, status, details) values (430,1,6,'2020-12-01','2020-12-06','Approved', 'Cập Nhật Ngày Nghỉ Nhân Viên');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1520, '2020-12-01', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1520, '2020-12-02', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1520, '2020-12-03', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1520, '2020-12-04', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1520, '2020-12-05', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1520, '2020-12-06', 'Half Day - Morning');
+insert into EmployeeLeaves (employee, leave_type, leave_period, date_start, date_end, status, details) values (431,1,6,'2020-12-01','2020-12-02','Approved', 'Cập Nhật Ngày Nghỉ Nhân Viên');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1521, '2020-12-01', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1521, '2020-12-02', 'Half Day - Morning');
+insert into EmployeeLeaves (employee, leave_type, leave_period, date_start, date_end, status, details) values (436,1,6,'2020-12-01','2020-12-11','Approved', 'Cập Nhật Ngày Nghỉ Nhân Viên');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1522, '2020-12-01', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1522, '2020-12-02', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1522, '2020-12-03', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1522, '2020-12-04', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1522, '2020-12-05', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1522, '2020-12-06', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1522, '2020-12-07', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1522, '2020-12-08', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1522, '2020-12-09', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1522, '2020-12-10', 'Full Day');
+INSERT INTO EmployeeLeaveDays (employee_leave, leave_date, leave_type)
+VALUES (1522, '2020-12-11', 'Full Day');
